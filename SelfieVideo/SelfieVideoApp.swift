fa429b884230f15948d1525ebb22cd8d00bc5b18
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-03 20:12:20 +0800

    Initial Commit

---
+++ SelfieVideo/SelfieVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfieVideoApp.swift
+//  SelfieVideo
+//
+//  Created by Andriyanto Halim on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SelfieVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
